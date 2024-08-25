52c1743f321f67361e101801e2ae8738b80d1320
chemyam <cmmcghee@valdosta.edu>
2024-08-25 01:21:34 -0400

    Add files via upload

--- ver2/Account.java
+++ ver2/Account.java
@@ -5,12 +5,6 @@ public class Account {
 	private double balance;  // New comment for balance :)
 	private String name;
 
-	
-	// Constructor
-	public Account(double balance) {
-		this("Unknown", balance);
-	}
-
 
 	
 	public Account(String name, double balance) {
